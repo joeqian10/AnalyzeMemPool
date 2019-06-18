ed3e5b8655ccf49a9f0d06758b96bd17a16e88ce
zhuoqian <zhuoqian10@gmail.com>
2019-06-18 15:24:28 +0800

    first commit

---
+++ AnalyzeMemPool/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace AnalyzeMemPool
+{
+    class Program
+    {
+        private static IConfigurationSection mainSection => new ConfigurationBuilder().AddJsonFile("appConfig.json").Build().GetSection("ApplicationConfiguration");
+        private static string AnalysisPath => mainSection.GetSection("AnalysisPath").Value;
+        private static string TableHeader => mainSection.GetSection("TableHeader").Value;
+
+        static void Main(string[] args)
+        {
+            string[] seeds = mainSection.GetSection("SeedList").GetChildren().Select(p => p.Value).ToArray();
+            int NodeNum = seeds.Length;
+            Console.WriteLine("Mempool analysis started...");
+
+            if (!File.Exists(AnalysisPath))
+            {
+                WriteLineToCSV(TableHeader);
+            }
+
+            int[] txNums = new int[NodeNum];
+            string[] urls = new string[NodeNum];
+
+            for (int i = 0; i < NodeNum; i++)
+            {
+                urls[i] = @"http://" + seeds[i];
+            }
+
+            bool stop = false;
+
+            Task task = Task.Run(() =>
+            {
+                while (!stop)
+                {
+                    for (int i = 0; i < NodeNum; i++)
+                    {
+                        txNums[i] = Tools.GetMemPoolTransNum(urls[i]);
+                    }
+                    DateTime time = DateTime.Now;
+                    WriteLineToCSV(time, txNums);
+//#if DEBUG
+                    WriteToConsole(time, txNums);
+//#endif
+                    Thread.Sleep(60000); // sleep for 1 minute 
+                }
+            });
+            Console.ReadLine();
+            stop = true;
+            task.Wait();
+
+            Console.WriteLine("Mempool analysis stopped...");
+        }
+
+        private static void WriteLineToCSV(string line)
+        {
+            using (FileStream fs = new FileStream(AnalysisPath, FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs, Encoding.ASCII))
+                sw.WriteLine(line);
+        }
+
+        private static void WriteLineToCSV(DateTime datetime, int[] txNum)
+        {
+            int len = txNum.Length;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(datetime).Append(",");
+            for (int i = 0; i <= len-2; i++)
+            {
+                sb.Append(txNum[i]).Append(",");
+            }
+            sb.Append(txNum[len - 1]);
+            WriteLineToCSV(sb.ToString());
+        }
+
+        private static void WriteToConsole(DateTime time, int[] txNum)
+        {
+            int len = txNum.Length;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(time).Append("\t");
+            for (int i = 0; i <= len - 2; i++)
+            {
+                sb.Append(txNum[i]).Append("\t");
+            }
+            sb.Append(txNum[len - 1]);
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ AnalyzeMemPool/Tools.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace AnalyzeMemPool
+{
+    public static class Tools
+    {
+        public static string HttpPost(string Url, string postData, int timeOut = 60000)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    WebRequest request = WebRequest.Create(Url);
+                    request.Method = "POST";
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                    request.ContentLength = byteArray.Length;
+                    request.Timeout = timeOut;
+                    Stream dataStream = request.GetRequestStream();
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Close();
+                    WebResponse response = request.GetResponse();
+                    dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+                    return responseFromServer;
+                }
+                catch (Exception we)
+                {
+                    Console.WriteLine(we.Message);
+                    Console.WriteLine("Time out，retrying……");
+                    continue;
+                }
+            }
+            return "";
+        }
+
+        public static int GetMemPoolTransNum(string url)
+        {
+            int count = 0;
+
+            try
+            {
+                var json = HttpPost(url, $@"
+                {{
+                    'jsonrpc': '2.0',
+                    'method': 'getrawmempool',
+                    'params': [],
+                    'id': 1
+                }}");
+                JToken a = JObject.Parse(json)["result"];
+
+                foreach (var tx in a.Values())
+                {
+                    count++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return count;
+            
+        }
+    }
+}
